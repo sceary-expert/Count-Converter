40a41efad37efc236bf65cfada4cb86ce20efcf2
Dhananjoy <deydhananjoy5@gmail.com>
2022-11-28 18:20:30 +0530

    Added all possoble count system

--- script.js
+++ script.js
@@ -12,26 +12,501 @@ function myFunction() {
     console.log(toCount);
     if(fromCount == "tex")
     {
-      if(toCount == "denier")
+      if(toCount == "tex")
+      {
+
+      }
+      else if(toCount == "denier")
       {
         let output = 9 * inputNumber;
         console.log(output);
-        var code = `<div><h3>The weight of 1000m material is ${inputNumber} g</h3><h3>The weight of 1m material is ${inputNumber} / 1000 g</h3><h3>The weight of 9000m material is ${output} g</h3></div>`;
+        var code = `<div>
+        <h3> ${inputNumber} Tex = ${output} Denier</h3>
+        <h3>Proof : <h3>
+        <h3>The weight of 1000m material is ${inputNumber} g</h3>
+        <h3>The weight of 1m material is ${inputNumber} / 1000 g</h3>
+        <h3>The weeight of 9000m material is 9000 * ${inputNumber} / 1000 g</h3>
+        <h3>The weight of 9000m material is ${output} g</h3>
+        </div>`;
         document.querySelector("body").innerHTML = code;
         
+      }
+      else if(toCount == "grist")
+      {
+
+      }
+      else if(toCount  == "english cotton count")
+      {
+
+      }
+      else if(toCount == "worsted count")
+      {
+
+      }
+      else if(toCount == "metric count")
+      {
+
+      }
+      else if(toCount == "linen count")
+      {
+
       }
       else if(toCount == "woolen count")
       {
 
       }
-      else if(toCount  == "")
+      else{
+        var code = `<div>
+        <h3> ${toCount} is not a valid count system</h3> 
+        </div>`;
+        document.querySelector("body").innerHTML = code;
+      }
+// Tex
+// Denier
+// Grist
+// English Cotton Count
+// Worsted Count
+// Metric Count
+// Linen Count
+// Wollen Count
+    }
+    else if(fromCount == "denier")
+    {
+      if(toCount == "tex")
       {
 
       }
-    }
-    else if(fromCount == "")
-    {
+      else if(toCount == "denier")
+      {
+        let output = 9 * inputNumber;
+        console.log(output);
+        var code = `<div>
+        
+        </div>`;
+        document.querySelector("body").innerHTML = code;
+        
+      }
+      else if(toCount == "grist")
+      {
 
+      }
+      else if(toCount  == "english cotton count")
+      {
+
+      }
+      else if(toCount == "worsted count")
+      {
+
+      }
+      else if(toCount == "metric count")
+      {
+
+      }
+      else if(toCount == "linen count")
+      {
+
+      }
+      else if(toCount == "woolen count")
+      {
+
+      }
+      else{
+        var code = `<div>
+        <h3> ${toCount} is not a valid count system</h3> 
+        </div>`;
+        document.querySelector("body").innerHTML = code;
+      }
+
+    }
+    else if(fromCount == "grist")
+    {
+      if(toCount == "tex")
+      {
+
+      }
+      else if(toCount == "denier")
+      {
+        let output = 9 * inputNumber;
+        console.log(output);
+        var code = `<div>
+        
+        </div>`;
+        document.querySelector("body").innerHTML = code;
+        
+      }
+      else if(toCount == "grist")
+      {
+
+      }
+      else if(toCount  == "english cotton count")
+      {
+
+      }
+      else if(toCount == "worsted count")
+      {
+
+      }
+      else if(toCount == "metric count")
+      {
+
+      }
+      else if(toCount == "linen count")
+      {
+
+      }
+      else if(toCount == "woolen count")
+      {
+
+      }
+      else{
+        var code = `<div>
+        <h3> ${toCount} is not a valid count system</h3> 
+        </div>`;
+        document.querySelector("body").innerHTML = code;
+      }
+
+    }
+    else if(fromCount == "english cotton count")
+    {
+      if(toCount == "tex")
+      {
+
+      }
+      else if(toCount == "denier")
+      {
+        let output = 9 * inputNumber;
+        console.log(output);
+        var code = `<div>
+        
+        </div>`;
+        document.querySelector("body").innerHTML = code;
+        
+      }
+      else if(toCount == "grist")
+      {
+
+      }
+      else if(toCount  == "english cotton count")
+      {
+
+      }
+      else if(toCount == "worsted count")
+      {
+
+      }
+      else if(toCount == "metric count")
+      {
+
+      }
+      else if(toCount == "linen count")
+      {
+
+      }
+      else if(toCount == "woolen count")
+      {
+
+      }
+      else{
+        var code = `<div>
+        <h3> ${toCount} is not a valid count system</h3> 
+        </div>`;
+        document.querySelector("body").innerHTML = code;
+      }
+
+    }
+    else if(fromCount == "worsted count")
+    {
+      if(toCount == "tex")
+      {
+
+      }
+      else if(toCount == "denier")
+      {
+        let output = 9 * inputNumber;
+        console.log(output);
+        var code = `<div>
+        
+        </div>`;
+        document.querySelector("body").innerHTML = code;
+        
+      }
+      else if(toCount == "grist")
+      {
+
+      }
+      else if(toCount  == "english cotton count")
+      {
+
+      }
+      else if(toCount == "worsted count")
+      {
+
+      }
+      else if(toCount == "metric count")
+      {
+
+      }
+      else if(toCount == "linen count")
+      {
+
+      }
+      else if(toCount == "woolen count")
+      {
+
+      }
+      else{
+        var code = `<div>
+        <h3> ${toCount} is not a valid count system</h3> 
+        </div>`;
+        document.querySelector("body").innerHTML = code;
+      }
+
+    }
+    else if(fromCount  == "metric count")
+    {
+      if(toCount == "tex")
+      {
+
+      }
+      else if(toCount == "denier")
+      {
+        let output = 9 * inputNumber;
+        console.log(output);
+        var code = `<div>
+        
+        </div>`;
+        document.querySelector("body").innerHTML = code;
+        
+      }
+      else if(toCount == "grist")
+      {
+
+      }
+      else if(toCount  == "english cotton count")
+      {
+
+      }
+      else if(toCount == "worsted count")
+      {
+
+      }
+      else if(toCount == "metric count")
+      {
+
+      }
+      else if(toCount == "linen count")
+      {
+
+      }
+      else if(toCount == "woolen count")
+      {
+
+      }
+      else{
+        var code = `<div>
+        <h3> ${toCount} is not a valid count system</h3> 
+        </div>`;
+        document.querySelector("body").innerHTML = code;
+      }
+
+    }
+    else if(fromCount == "metric count")
+    {
+      if(toCount == "tex")
+      {
+
+      }
+      else if(toCount == "denier")
+      {
+        let output = 9 * inputNumber;
+        console.log(output);
+        var code = `<div>
+        
+        </div>`;
+        document.querySelector("body").innerHTML = code;
+        
+      }
+      else if(toCount == "grist")
+      {
+
+      }
+      else if(toCount  == "english cotton count")
+      {
+
+      }
+      else if(toCount == "worsted count")
+      {
+
+      }
+      else if(toCount == "metric count")
+      {
+
+      }
+      else if(toCount == "linen count")
+      {
+
+      }
+      else if(toCount == "woolen count")
+      {
+
+      }
+      else{
+        var code = `<div>
+        <h3> ${toCount} is not a valid count system</h3> 
+        </div>`;
+        document.querySelector("body").innerHTML = code;
+      }
+
+    }
+    else if(fromCount == "metric count")
+    {
+      if(toCount == "tex")
+      {
+
+      }
+      else if(toCount == "denier")
+      {
+        let output = 9 * inputNumber;
+        console.log(output);
+        var code = `<div>
+        
+        </div>`;
+        document.querySelector("body").innerHTML = code;
+        
+      }
+      else if(toCount == "grist")
+      {
+
+      }
+      else if(toCount  == "english cotton count")
+      {
+
+      }
+      else if(toCount == "worsted count")
+      {
+
+      }
+      else if(toCount == "metric count")
+      {
+
+      }
+      else if(toCount == "linen count")
+      {
+
+      }
+      else if(toCount == "woolen count")
+      {
+
+      }
+      else{
+        var code = `<div>
+        <h3> ${toCount} is not a valid count system</h3> 
+        </div>`;
+        document.querySelector("body").innerHTML = code;
+      }
+
+    }
+    else if(fromCount == "linen count")
+    {
+      if(toCount == "tex")
+      {
+
+      }
+      else if(toCount == "denier")
+      {
+        let output = 9 * inputNumber;
+        console.log(output);
+        var code = `<div>
+        
+        </div>`;
+        document.querySelector("body").innerHTML = code;
+        
+      }
+      else if(toCount == "grist")
+      {
+
+      }
+      else if(toCount  == "english cotton count")
+      {
+
+      }
+      else if(toCount == "worsted count")
+      {
+
+      }
+      else if(toCount == "metric count")
+      {
+
+      }
+      else if(toCount == "linen count")
+      {
+
+      }
+      else if(toCount == "woolen count")
+      {
+
+      }
+      else{
+        var code = `<div>
+        <h3> ${toCount} is not a valid count system</h3> 
+        </div>`;
+        document.querySelector("body").innerHTML = code;
+      }
+
+    }
+    else if(fromCount  == "woolen count")
+    {
+      if(toCount == "tex")
+      {
+
+      }
+      else if(toCount == "denier")
+      {
+        let output = 9 * inputNumber;
+        console.log(output);
+        var code = `<div>
+        
+        </div>`;
+        document.querySelector("body").innerHTML = code;
+        
+      }
+      else if(toCount == "grist")
+      {
+
+      }
+      else if(toCount  == "english cotton count")
+      {
+
+      }
+      else if(toCount == "worsted count")
+      {
+
+      }
+      else if(toCount == "metric count")
+      {
+
+      }
+      else if(toCount == "linen count")
+      {
+
+      }
+      else if(toCount == "woolen count")
+      {
+
+      }
+      else{
+        var code = `<div>
+        <h3> ${toCount} is not a valid count system</h3> 
+        </div>`;
+        document.querySelector("body").innerHTML = code;
+      }
+
+    }
+    else{
+      var code = `<div>
+        <h3> ${fromCount} is not a valid count system</h3> 
+        </div>`;
+        document.querySelector("body").innerHTML = code;
     }
     return 
   }
